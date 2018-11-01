5a53ef83f410bb9bec8e3a5b63a720e028c05d83
jihoon <thtjwls@gmail.com>
2018-11-01 09:00:06 +0900

    메뉴 컴포넌트 추가

---
+++ src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/about/foo">About foo</Link>
+                </li>
+            </ul>
+            <hr />
+        </div>
+    );
+}
+
+export default Menu;
