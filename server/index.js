b56671654881892891835380ae9a1dcf9f98269a
jihoon <thtjwls@gmail.com>
2018-11-02 08:57:46 +0900

    서버사이드 렌더링 - 새로고침 할 때 페이지 못 불러오는 부분

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa');
 const serve = require('koa-static');
 const path = require('path');
+const fs = require('fs');
 const app = new Koa();
+
+const indexHtml = fs.readFileSync(path.resolve(__dirname, '../build/index.html'), { encoding: 'utf8'});
 
 app.use(serve(path.resolve(__dirname, '../build')));
 app.use(ctx => {
