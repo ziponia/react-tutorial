96ac45a6b55934d9a258d896d878763dca0ae0bc
jihoon <thtjwls@gmail.com>
2018-11-02 12:29:03 +0900

    and

--- server/render/index.js
+++ server/render/index.js
@@ -7,7 +7,7 @@ const template = fs.readFileSync(path.join(__dirname, '../../build/index.html'),
 
 module.exports = async ctx => {
     const location = ctx.path;
-    const { html, state } = await render(location);
+    const { html, state, helmet } = await render(location);
     const page = template.replace('<div id="root"></div>', `<div id="root">${html}</div><script>window.__PRELOADED_STATE__=${serialize(state)}</script>`)
                             .replace('<meta helmet>', `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}`);
     ctx.body = page;
