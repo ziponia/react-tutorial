4be16b1db05295ce3f3ad12c0a5693c745e6a339
jihoon <thtjwls@gmail.com>
2018-11-02 12:22:32 +0900

    서버사이드 렌더링 - 페이지 helmet 설정

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import qs from 'query-string';
+import { Helmet } from 'react-helmet';
 
 const About = ({match, location}) => {
     const query = qs.parse(location.search);
     const detail = query.detail === 'true';
+    const { name } = match.params;
 
     return (
         <div>
+            <Helmet>
+                <title>{`About ${name ? name : ''}`}</title>
+            </Helmet>
             <h2>
                 About {match.params.name}
                 {detail && 'detail: adfafd'}
