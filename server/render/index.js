f265a8363352976633e5f310eb507c8c33128a11
jihoon <thtjwls@gmail.com>
2018-11-02 12:17:46 +0900

    서버 사이드 렌더링 - JSON.stringify 안정성 향상

--- server/render/index.js
+++ server/render/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const render = require('./render').default;
+const serialize = require('serialize-javascript');
 
 const template = fs.readFileSync(path.join(__dirname, '../../build/index.html'), { encoding: 'utf8' });
 
 module.exports = async ctx => {
     const location = ctx.path;
     const { html, state } = await render(location);
-    const page = template.replace('<div id="root"></div>', `<div id="root">${html}</div><script>window.__PRELOADED_STATE__=${JSON.stringify(state)}</script>`);
+    const page = template.replace('<div id="root"></div>', `<div id="root">${html}</div><script>window.__PRELOADED_STATE__=${serialize(state)}</script>`);
     ctx.body = page;
 }
