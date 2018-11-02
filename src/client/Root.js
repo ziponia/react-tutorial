29ae9aefb1b35232dd1ba370cd944467c477c87d
jihoon <thtjwls@gmail.com>
2018-11-02 09:46:30 +0900

    서버 사이드 렌더링 - 리덕스 연동

--- src/client/Root.js
+++ src/client/Root.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import {BrowserRouter} from 'react-router-dom';
 import App from '../shared/App';
+import { Provider } from 'react-redux';
+
+import store from '../../redux/store';
 
 class Root extends Component {
   render() {
     return (
       <BrowserRouter>
+      <Provider store={store}>
         <App />
+        </Provider>
       </BrowserRouter>
     )
   }
---
+++ src/redux/configureStore.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
+
+import modules from './modules';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const composeEnhancers = isDevelopment ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
+
+const configureStore = initialState => {
+    const store = createStore(moduels, initialState, composeEnhancers(
+        applyMiddleware(penderMiddleware())
+    ));
+
+    if (module.hot) {
+        module.hot.accept('./modules', () => {
+            const nextRootReducer = require('./modules').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+
+    return store;
+}
+
+export default configureStore;
