04161a296199f0a5ad69552f07e311443b23e6ba
jihoon <thtjwls@gmail.com>
2018-11-02 10:29:39 +0900

    서버사이드 렌더링 - 프리로더 http://127.0.0.1:3001/users 접속해서 보면 __PRELOADED_STATE__={ 이곳에서 데이터가 로딩 된 것을 확인 할 수 있음}

--- server/render/index.js
+++ server/render/index.js
@@ -4,9 +4,9 @@ const render = require('./render').default;
 
 const template = fs.readFileSync(path.join(__dirname, '../../build/index.html'), { encoding: 'utf8' });
 
-module.exports = ctx => {
+module.exports = async ctx => {
     const location = ctx.path;
-    const rendered = render(location);
-    const page = template.replace('<div id="root"></div>', `<div id="root">${rendered}</div>`);
+    const { html, state } = await render(location);
+    const page = template.replace('<div id="root"></div>', `<div id="root">${html}</div><script>window.__PRELOADED_STATE__=${JSON.stringify(state)}</script>`);
     ctx.body = page;
 }
--- src/server/render.js
+++ src/server/render.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router';
 import App from '../shared/App';
 
-const render = (location) => ReactDOMServer.renderToString(
-    <StaticRouter location={location}>
-        <App />
-    </StaticRouter>
-);
+import { Provider } from 'react-redux';
+
+import { renderToString } from 'react-router-server';
+import configureStore from '../redux/configureStore';
+
+const render = async (location) => {
+    const store = configureStore();
+
+    const { html } = await renderToString(
+        <StaticRouter location={location}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </StaticRouter>
+    );
+
+    return {
+        html, state: store.getState()
+    }
+}
 
 export default render;
